7c666e39a1f381b0da44c63adf7cf35a72f32f2c
phortuin <phortuin@users.noreply.github.com>
2021-07-19 20:37:27 +0200

    feat: add icon component

---
+++ src/components/icon/icon.svelte
@@ -0,0 +1,19 @@
+<script>
+	import add from './add.svg'
+	import edit from './edit.svg'
+	import settings from './settings.svg'
+	import close from './close.svg'
+
+	let icons = {
+		add,
+		edit,
+		settings,
+		close
+	}
+
+	export let name
+
+	$: icon = icons[name]
+</script>
+
+{@html icon}
