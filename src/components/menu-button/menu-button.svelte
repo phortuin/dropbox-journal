1ff359e85aa3fc92f6bf4c08a00428ff33f42546
phortuin <phortuin@users.noreply.github.com>
2021-07-19 20:37:43 +0200

    feat: add menubutton component

---
+++ src/components/menu-button/menu-button.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Link from '../link/link.svelte'
+	import Icon from '../icon/icon.svelte'
+
+	export let path = '#', icon, label
+</script>
+
+<Link
+	href="{ path }">
+	<Icon name="{ icon }" />
+	{ label }
+</Link>
